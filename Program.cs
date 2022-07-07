dfc946139b7d72717da2f04f61c59a74d62308bb
Samandar <uchqunovsamandar@mail.ru>
2022-07-06 23:47:27 -0700

    configure to signalr

--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddSignalR();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -17,9 +20,5 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
+app.MapHub<MainHub>("/current-time");
 app.Run();
